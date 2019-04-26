9b0bec9a869fbbfdf2993fd05290988658775cad
jifukui <805013833@qq.com>
2019-04-26 15:21:48 +0800

    编译执行直接插入排序

--- 直接插入排序/main.c
+++ 直接插入排序/main.c
@@ -5,11 +5,13 @@ int main()
     int data[]={12,5,8,65,78,32,15,1,45,66,27,56,77,99};
     int i,n;
     int temp;
-    for(i=2;i<13;i++)
+    for(i=1;i<14;i++)
     {
         temp=data[i];
-        n=i-1;
-        while(tmp<data[n])
+		//printf("The %i is %d\n",i,temp);
+       	n=i-1;
+		//注意除了需要小于当前边界的值还需要注意范围
+        while(temp<data[n]&&(n>-1))
         {
             data[n+1]=data[n];
             n--;
