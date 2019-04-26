812cb54157cefc2ffab9f2d41bc7cfe54cc093dd
jifukui <805013833@qq.com>
2019-04-26 14:33:23 +0800

    修改编译冒泡排序

--- 冒泡排序/main.c
+++ 冒泡排序/main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <sys/time.h>
-int main
+int main()
 {
-    int data[]={12,5,8,65,78,32,5,1,45,66,27,56,77,99};
+    int data[]={12,5,8,65,78,32,15,1,45,66,27,56,77,99};
     int i,n;
     int temp;
-    for(i=0;i<((sizeof data)/(sizeof(int))-1;i++)
+    for(i=0;i<13;i++)
     {
-        for(n=0;n<((sizeof data)/(sizeof(int))-1;n++)
+        for(n=0;n<13;n++)
         {
             if(data[n]>data[n+1])
             {
@@ -17,7 +17,7 @@ int main
             }
         }
     }
-    for(n=0;n<n<((sizeof data)/(sizeof(int));n++)
+    for(n=0;n<14;n++)
     {
         printf("The %d Is %d \n",n,data[n]);
     }
