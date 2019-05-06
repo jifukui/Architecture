5064782fa20bea70869a5fdcfede6b4046f8a24a
jifukui <805013833@qq.com>
2019-05-06 15:55:46 +0800

    添加栈的顺序存储

--- 栈/顺序存储/stack.c
+++ 栈/顺序存储/stack.c
@@ -55,7 +55,7 @@ bool StackFull(sStack *stack)
 }
 bool Push(sStack *stack,int value)
 {
-    if(stack&&(MAX-1)>stack->top)
+    if(stack&&((MAX-1)>stack->top))
     {
         stack->data[++stack->top]=value;
         return TRUE;
