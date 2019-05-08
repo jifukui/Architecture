d77a22f3b166830b3e6a6f000ad38281d866081a
jifukui <805013833@qq.com>
2019-05-08 08:48:18 +0800

    添加队列顺序存储

--- 队列/顺序存储/queue.c
+++ 队列/顺序存储/queue.c
@@ -47,7 +47,7 @@ int QueueFull(squeue *queue)
 {
     if(queue)
     {
-        if(((queue->tail+2)&QueueMask)==queue->head)
+        if(((queue->tail+1)&QueueMask)==queue->head)
         {
             return TRUE;
         }
