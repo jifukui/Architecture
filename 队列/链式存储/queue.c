0b6204d23e71c99015312fa37645d09aa4bbc178
jifukui <805013833@qq.com>
2019-05-08 09:51:45 +0800

    添加队列链式存储

---
+++ 队列/链式存储/queue.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "queue.h"
+static const unsigned int QueueMask=MAX-1;
+squeue *QueueInit()
+{
+    squeue *queue=NULL;
+    queue=(squeue *)malloc(sizeof(squeue));
+    if(queue)
+    {
+        queue->head=queue->tail=NULL;
+        length=0;
+    }
+    return queue;
+}
+void QueueDestory(squeue *queue)
+{
+    if(queue)
+    {
+        free(queue);
+        queue=NULL;
+    }
+}
+void QueueClear(squeue *queue)
+{
+    int len;
+    sQueueNode *node=queue->head;
+    sQueueNode *current;
+    len=queue->length;
+    while ((len--)&&node)
+    {
+        current=node->next;
+        free(node);
+        node=current;
+    }
+    
+}
+int QueueLength(squeue *queue)
+{
+    if(queue)
+    {
+        return queue->length;
+    }
+    return QUEUE_ERR;
+}
+int QueueEmpty(squeue *queue)
+{
+    if(queue)
+    {
+        if(0==queue->length)
+        {
+            return TRUE;
+        }
+    }
+    return FALSE;
+}
+int QueueFull(squeue *queue)
+{
+    if(queue)
+    {
+        if(MAX==queue->length)
+        {
+            return TRUE;
+        }
+    }
+    return FALSE;
+}
+int EnQueue(squeue *queue,int value)
+{
+    if(queue&&!QueueFull(queue))
+    {
+        sQueueNode *node=NULL;
+        node=(sQueueNode*)malloc(sizeof(sQueueNode));
+        if(node)
+        {
+            node->data=value;
+            node->next=NULL;
+            ((sQueueNode *)(queue->tail))->next=node;
+            queue->length++;
+            return TRUE;
+        }
+    }
+    return FALSE;
+}
+int DeQueue(squeue *queue)
+{
+    if(queue)
+    {
+        int value;
+        value=GetHeadQueue(queue);
+        if(value)
+        {
+            sQueueNode *node=NULL;
+            node=queue->head;
+            queue->length--;
+        }
+        return value;
+    }
+    return FALSE;
+}
+int GetHeadQueue(squeue *queue)
+{
+    if(queue&&(!QueueEmpty(queue)))
+    {
+        return queue->data[queue->head];
+    }
+    return FALSE;
+}
+void QueueDisplay(squeue*queue)
+{
+    if(queue)
+    {
+        int i=0;
+        int index;
+        index=queue->length;
+        sQueueNode *node=NULL;
+        while((index--)&&node)
+        {
+            printf("The %d is %d\n",i,node->data);
+            node=node->next;
+        }
+    }
+}
---
+++ 队列/链式存储/queue.h
@@ -0,0 +1,28 @@
+#ifndef _QUEUE_H
+#define _QUEUE_H
+#define MAX 4
+#define QUEUE_OK 0
+#define QUEUE_ERR -1
+#define TRUE 1
+#define FALSE 0
+typedef struct queue
+{
+    struct sQueueNode* head,*tail;
+    int length;
+}squeue,*psqueue;
+typedef struct queuenode
+{
+    int data;
+    struct sQueueNode* next;  
+}sQueueNode,*spQueueNode;
+squeue *QueueInit();
+void QueueDestory(squeue *queue);
+void QueueClear(squeue *queue);
+int QueueLength(squeue *queue);
+int QueueEmpty(squeue *queue);
+int QueueFull(squeue *queue);
+int EnQueue(squeue *queue,int value);
+int DeQueue(squeue *queue);
+int GetHeadQueue(squeue *queue);
+void QueueDisplay(squeue*queue);
+#endif
