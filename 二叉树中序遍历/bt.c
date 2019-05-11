397a9bfe4f6f91e5e9a17e00a5479a7411484918
jifukui <805013833@qq.com>
2019-05-11 18:00:25 +0800

    添加二叉树中序遍历

---
+++ 二叉树中序遍历/bt.c
@@ -0,0 +1,163 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "bt.h"
+#include "stack.h"
+#include "queue.h"
+const int BTMask=MAX;
+sBinaryTree* StoreTreeInit()
+{
+    sBinaryTree * bt=NULL;
+    bt=(sBinaryTree*)malloc(sizeof(sBinaryTree));
+    bt->length=0;
+    if(bt)
+    {
+        bt->queue=NULL;
+        bt->queue=QueueInit();
+        if(bt->queue)
+        {
+            return bt;
+        }
+    }
+    return NULL;
+}
+void StoreTreeDestory(sBinaryTree *bt)
+{
+    if(bt)
+    {
+        free(bt);
+        bt=NULL;
+    }
+}
+int StoreTree(sBinaryTree *bt,int value)
+{
+    if(bt)
+    {
+        sBinaryTreeNode *node=NULL;
+        sBinaryTreeNode *pnode=NULL;
+        node=(sBinaryTreeNode *)malloc(sizeof(sBinaryTreeNode));
+        if(bt->length<BTMask&&node)
+        { 
+            node->data=value;
+            node->lchild=node->rchild=NULL;
+			if(EnQueue(bt->queue,node))
+            {
+                if(!bt->length)
+                {
+                    bt->root=node;
+                }
+                else
+                {
+                    pnode=GetHeadQueue(bt->queue);
+                    if(node->data!='@'&&pnode->data!='@')
+                    {
+                        if(bt->length%2)
+                        {
+                            pnode->lchild=node;
+                        }
+                        else
+                        {
+                            pnode->rchild=node;
+                        }   
+                    }
+                    
+                }
+            }
+			if ((bt->length % 2) == 0&&bt->length!=0)
+			{
+				DeQueue(bt->queue);
+			}
+			bt->length++;
+			return BT_OK;
+        }
+    }
+    printf("store Tree Error %c\n",value);
+    return BT_ERR;
+}
+void TreeFrontErgodic(sBinaryTree *bt)
+{
+    if(bt)
+    {
+        sStack *fstack=NULL;
+        fstack=StackInit();
+        if(!fstack)
+        {
+            return NULL;
+        }
+        sBinaryTreeNode *node=NULL;
+        int len=bt->length;
+        node=bt->root;
+        int index=0;
+        int data;
+        Push(fstack,node);
+        while (!StackEmpty(fstack))
+        {
+            node=Pop(fstack);
+            while (node&&node->data!='@')
+            {
+                data=node->data;
+                printf("The %d is %c\n",index,data);
+                index++;
+                if(node->rchild)
+                {
+                    Push(fstack,node->rchild);
+                }
+                node=node->lchild;
+            }
+            
+        }
+        
+    }
+    return NULL;
+}
+void TreeMidErgodic(sBinaryTree *bt)
+{
+    if(bt)
+    {
+        sStack *fstack=NULL;
+        fstack=StackInit();
+        if(!fstack)
+        {
+            return NULL;
+        }
+        sBinaryTreeNode *node=NULL;
+        int len=bt->length;
+        node=bt->root;
+        int index=0;
+        int data;
+        Push(fstack,node);
+        while (!StackEmpty(fstack))
+        {
+            node=Pop(fstack);
+            while (node&&node->data!='@')
+            {
+                index++;
+                if(node->lchild)
+                {
+                    Push(fstack,node);
+                }
+                node=node->lchild;
+            }
+            if(!StackEmpty(fstack))
+            {
+                data=node->data;
+                printf("The %d is %c\n",index,data);
+            }
+            Push(fstack,node->rchild);
+        }
+        
+    }
+    return NULL;
+}
+void DisplayStoreTree(sBinaryTree *bt)
+{
+    if(bt)
+    {
+        int i,index=0;
+        i=bt->length;
+        while (i--)
+        {
+            //printf("The %d is %c \n",index,bt->data[index]);
+            index++;
+        }
+    }
+}
