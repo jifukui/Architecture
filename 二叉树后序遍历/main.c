4e455ee9806945091e093dc3074045d31591e24f
jifukui <805013833@qq.com>
2019-05-11 20:56:34 +0800

    二叉树后序遍历

---
+++ 二叉树后序遍历/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "bt.h"
+int main()
+{
+    int data;
+    sBinaryTree *bt;
+    bt=StoreTreeInit();
+    if(bt)
+    {
+        printf("初始华顺序二叉树成功\n");
+        printf("输入数据\n");
+        data=getchar();
+        while (data!='#')
+        {
+            printf("输入数据是:%c\n",data);
+            if(!StoreTree(bt,data))
+            {
+                break;
+            }
+            printf("输入数据\n");
+            getchar();
+            data=getchar();
+        }
+        printf("前序遍历树为：\n");
+        TreeFrontErgodic(bt);
+        printf("后续遍历树为:\n");
+        TreeMidErgodic(bt);
+        StoreTreeDestory(bt);
+    }
+    else
+    {
+        printf("初始华顺序二叉树失败\n");
+    }
+    return 0;
+}
