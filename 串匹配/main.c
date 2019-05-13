b4b0f9203aeab125e6b17087c5177a4ffdc7ad04
jifukui <805013833@qq.com>
2019-05-13 15:40:02 +0800

    添加串的kmp算法

--- 串匹配/main.c
+++ 串匹配/main.c
@@ -5,9 +5,9 @@ int main(int argc,char **argv)
     if(argc==3)
     {
         int pos=-1;
-        printf("父字符串是:%s\n",*argv[1]);
-        printf("模式串是%s\n",*argv[2]);
-        if(strlen(*argv[1])<strlen(*argv[2]))
+        printf("父字符串是:%s\n",argv[1]);
+        printf("模式串是%s\n",argv[2]);
+        if(strlen(argv[1])<strlen(argv[2]))
         {
             printf("错误的匹配串\n");
             return 0;
