9caa6d750ca1c12ac9ce2851c62a0f8f7d74542b
jifukui <805013833@qq.com>
2019-05-15 16:04:46 +0800

    添加中序线索二叉树

---
+++ 线索二叉树/bt.c
@@ -0,0 +1,337 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "bt.h"
+#include "stack.h"
+#include "istack.h"
+#include "queue.h"
+const int BTMask=MAX;
+sBinaryTree* StoreTreeInit()
+{
+    sBinaryTree * bt=NULL;
+    bt=(sBinaryTree*)malloc(sizeof(sBinaryTree));
+    bt->length=0;
+    if(bt)
+    {
+        bt->queue=NULL;
+        bt->queue=QueueInit();
+        if(bt->queue)
+        {
+            return bt;
+        }
+    }
+    return NULL;
+}
+void StoreTreeDestory(sBinaryTree *bt)
+{
+    if(bt)
+    {
+        free(bt);
+        bt=NULL;
+    }
+}
+int StoreTree(sBinaryTree *bt,int value)
+{
+    if(bt)
+    {
+        sBinaryTreeNode *node=NULL;
+        sBinaryTreeNode *pnode=NULL;
+        node=(sBinaryTreeNode *)malloc(sizeof(sBinaryTreeNode));
+        if(bt->length<BTMask&&node)
+        { 
+            node->data=value;
+            node->flag=0;
+            node->lchild=node->rchild=NULL;
+			if(EnQueue(bt->queue,node))
+            {
+                if(!bt->length)
+                {
+                    bt->root=node;
+                }
+                else
+                {
+                    pnode=GetHeadQueue(bt->queue);
+                    if(node->data!='@'&&pnode->data!='@')
+                    {
+                        if(bt->length%2)
+                        {
+                            pnode->lchild=node;
+                        }
+                        else
+                        {
+                            pnode->rchild=node;
+                        }   
+                    }
+                    
+                }
+            }
+			if ((bt->length % 2) == 0&&bt->length!=0)
+			{
+				DeQueue(bt->queue);
+			}
+			bt->length++;
+			return BT_OK;
+        }
+    }
+    printf("store Tree Error %c\n",value);
+    return BT_ERR;
+}
+void TreeFrontErgodic(sBinaryTree *bt)
+{
+    if(bt)
+    {
+        sStack *fstack=NULL;
+        fstack=StackInit();
+        if(!fstack)
+        {
+            return NULL;
+        }
+        sBinaryTreeNode *node=NULL;
+        int len=bt->length;
+        node=bt->root;
+        int index=0;
+        int data;
+        Push(fstack,node);
+        while (!StackEmpty(fstack))
+        {
+            node=Pop(fstack);
+            while (node&&node->data!='@')
+            {
+                data=node->data;
+                printf("The %d is %c\n",index,data);
+                index++;
+                if(node->rchild)
+                {
+                    Push(fstack,node->rchild);
+                }
+                node=node->lchild;
+            }
+            
+        }
+        
+    }
+    return NULL;
+}
+void TreeMidErgodic(sBinaryTree *bt)
+{
+    if(bt)
+    {
+        sStack *fstack=NULL;
+        fstack=StackInit();
+        if(!fstack)
+        {
+            return NULL;
+        }
+        sBinaryTreeNode *node=NULL;
+        int len=bt->length;
+        node=bt->root;
+        int index=0;
+        int data;
+        Push(fstack,node);
+        while (!StackEmpty(fstack))
+        {
+            node=Pop(fstack);
+            while (node&&node->data!='@')
+            {
+                Push(fstack,node);
+                node=node->lchild;
+            }
+            if(!StackEmpty(fstack))
+            {
+                node=Pop(fstack);
+                data=node->data;
+                printf("The %d is %c\n",index,data);
+                index++;
+                Push(fstack,node->rchild);
+            }
+            
+        }
+        
+    }
+    return NULL;
+}
+void TreeEedErgodic(sBinaryTree *bt)
+{
+    if(bt)
+    {
+        sStack *fstack=NULL;
+        fstack=StackInit();
+        if(!fstack)
+        {
+            return NULL;
+        }
+        sBinaryTreeNode *node=NULL;
+        int len=bt->length;
+        node=bt->root;
+        int index=0;
+        int data;
+        Push(fstack,node);
+        while (!StackEmpty(fstack))
+        {
+            node=Pop(fstack);
+            while (node&&node->data!='@')
+            { 
+                if(!node->flag)
+                {
+                    Push(fstack,node);
+                }
+				else
+				{
+					break;
+				}
+                node=node->lchild;
+            }
+			if (!StackEmpty(fstack))
+			{
+				node = Pop(fstack);
+				if (node->flag)
+				{
+					data = node->data;
+					printf("The %d is %c\n", index, data);
+					index++;	
+				}
+				else
+				{
+					node->flag = 1;
+					Push(fstack, node);
+				}	
+				Push(fstack, node->rchild);
+			}
+        }
+    }
+}
+
+int TreeDeep(sBinaryTree *bt)
+{
+    int maxdeep=0;
+    if(bt)
+    {
+        int deep=1;
+        isStack *dstack=NULL;
+        sStack *fstack=NULL;
+        fstack=StackInit();
+        dstack=IStackInit();
+        if(!fstack&&!dstack)
+        {
+            return maxdeep;
+        }
+        sBinaryTreeNode *node=NULL;
+        int len=bt->length;
+        node=bt->root;
+        int index=0;
+        int data;
+        Push(fstack,node);
+        IPush(dstack,deep);
+        while (!StackEmpty(fstack))
+        {
+            node=Pop(fstack);
+            deep=IPop(dstack);
+            while (node&&node->data!='@')
+            {
+                Push(fstack,node);
+                IPush(dstack,deep);
+                deep++;
+                node=node->lchild;
+            }
+            if(!StackEmpty(fstack))
+            {
+                node=Pop(fstack);
+                deep=IPop(dstack);
+                printf("当前的深度为%d\n",deep);
+                printf("当前的最大深度为%d\n",maxdeep);
+                if(deep>maxdeep)
+                {
+                    maxdeep=deep;
+                }
+                data=node->data;
+                printf("The %d is %c\n",index,data);
+                index++;
+                Push(fstack,node->rchild);
+                deep++;
+                IPush(dstack,deep);
+            }
+            
+        }
+        
+    }
+    return maxdeep;
+}
+
+/**线索二叉树中序存储*/
+scluetree *ClueTreeInit()
+{
+    scluetree *root=NULL;
+    root=(scluetree *)malloc(sizeof(scluetree));
+    return root;
+}
+scluetree * StoreClueTreeMid(sBinaryTree *bt)
+{
+    scluetree *root=NULL;
+    root=ClueTreeInit();
+    scluetreenode *cnode=NULL;
+    scluetreenode *pcnode=NULL;
+    int index=0;
+    if(root)
+    {
+        root->length=1;
+        root->top=bt->root;
+        sBinaryTreeNode *node;
+        sStack *stack=NULL;
+        Push(stack,node);
+        while (!StackEmpty(stack))
+        {
+            node=Pop(stack);
+            while (node&&node->data!='@')
+            {
+                Push(stack,node);
+                node=node->lchild;
+            }
+            if(!StackEmpty(stack))
+            {
+                node=Pop(stack);
+                cnode->data=node->data;
+                cnode=(scluetreenode *)malloc(sizeof(scluetreenode));
+                if(((sBinaryTreeNode *)(node->lchild))->data=='@')
+                {
+                    cnode->ltag=1;
+                    cnode->lchild=pcnode;
+                }
+                if(((sBinaryTreeNode *)(node->rchild))->data=='@')
+                {
+                    cnode->rtag=1;
+                }
+                if(pcnode&&pcnode->rtag==1)
+                {
+                    pcnode->rchild=cnode;
+                }
+                if(index==0)
+                {
+                    root->top=node;
+                }
+                pcnode=cnode;
+                Push(stack,node->rchild);
+            }
+        }
+        return root;
+    }
+    else
+    {
+        printf("中序存储线索二叉树失败\n");
+        return NULL;
+    }
+    
+}
+
+void DisplayStoreTree(scluetree *root)
+{
+    scluetreenode *cnode=NULL;
+    cnode=root->top;
+    if(cnode)
+    {
+        int i=0;
+        while (cnode)
+        {
+            printf("The %d is %c\n",i,cnode->data);
+            i++;
+        }
+    }
+}
---
+++ 线索二叉树/istack.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "istack.h"
+static const  int IStackMask=MAX;
+isStack * IStackInit()
+{
+    isStack *stack=NULL;
+    stack=(ipsStack)malloc(sizeof(isStack));
+    if(stack)
+    {
+        printf("栈创建成功\n");
+        stack->top=NULL;
+        stack->len=0;
+    }
+    return stack;
+}
+void IStackDestory(isStack *stack)
+{
+    if(stack)
+    {
+        IStackClear(stack);
+        free(stack);
+        stack=NULL;
+        printf("释放栈成功\n");
+    }
+}
+void IStackClear(isStack *stack)
+{
+    if(stack)
+    {
+        int len;
+        isStackNode *current,*pre;
+        current=stack->top;
+        len=stack->len;
+        while ((len--)&&current)
+        {
+            pre=current->pre;
+            free(current);
+            current=pre;
+        }
+        stack->len=0;
+    }
+}
+int  IStackLength(isStack *stack)
+{
+    if(stack)
+    {
+        return stack->len;
+    }
+    return STACK_ERR;
+}
+bool IStackEmpty(isStack *stack)
+{
+    if(stack&&(!stack->len))
+    {
+        return TRUE;
+    }
+    return FALSE;
+    
+}
+bool IStackFull(isStack *stack)
+{
+    if(stack&&(stack->len==IStackMask))
+    {
+        return TRUE;
+    }
+    return FALSE;
+}
+bool IPush(isStack *stack,int value)
+{
+    if(stack&&stack->len<IStackMask)
+    {
+        isStackNode *node=NULL;
+        node =(isStackNode*)malloc(sizeof(isStackNode));
+        if(NULL==node)
+        {
+            return FALSE;
+        }
+        node->data=value;
+        node->pre=stack->top;
+        stack->top=node;
+        stack->len++;
+        return TRUE;
+    }
+    return FALSE;
+}
+int  IPop(isStack *stack)
+{
+    if(stack&&stack->top>0)
+    {
+        int data;
+        isStackNode *current;
+        current=stack->top;
+        stack->len--;
+        data=current->data;
+        stack->top=current->pre;
+        free(current);
+        return data;
+    }
+    return STACK_ERR;
+}
+int IGetTop(isStack *stack)
+{
+    if(stack&&stack->top>0)
+    {
+        return ((isStackNode *)(stack->top))->data;
+    }
+    return STACK_ERR;
+}
+void IStackDisplay(isStack *stack)
+{
+    if(stack&&stack->top>0)
+    {
+        isStackNode *node=stack->top;
+        int len=stack->len;
+        int i=0;
+        while (len--&&node)
+        {
+            printf("The %d is %d\n",i,node->data);
+            node=node->pre;
+            i++;
+        }        
+    }
+}
