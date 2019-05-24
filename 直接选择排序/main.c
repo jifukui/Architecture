dedd1d29c7b9229afa81b051218c3a708dd4c802
jifukui <805013833@qq.com>
2019-05-24 16:39:40 +0800

    添加直接选择排序

--- 直接选择排序/main.c
+++ 直接选择排序/main.c
@@ -2,7 +2,7 @@
 int main()
 {
     int data[]={12,5,8,65,78,32,15,1,45,66,27,56,77,99};
-    int i,n,k;
+    int i,j,k;
     int temp;
     int  num=(sizeof data)/sizeof(int);
     for(i=0;i<num;i++)
