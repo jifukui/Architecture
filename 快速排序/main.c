c0dd6ee9c39d5d3dc6446a66213b61ce7bb68c44
jifukui <805013833@qq.com>
2019-05-24 14:19:18 +0800

    添加快速排序

---
+++ 快速排序/main.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include "stack.h"
+typedef struct 
+{
+    int low;
+    int height;
+}Qk;
+
+int qkpass(int *data,int low,int height);
+void qksort(int *data,int len);
+int main()
+{
+    int data[]={12,5,8,65,78,32,15,1,45,66,27,56,77,99};
+    int i,n;
+    int temp;
+    int num=(sizeof data)/sizeof(int);
+    qksort(data,num);
+    for(n=0;n<num;n++)
+    {
+        printf("The %d Is %d \n",n,data[n]);
+    }
+    return 0;
+}
+int qkpass(int *data,int low,int height)
+{
+    int i=low,j=height;
+    int temp=data[low];
+    while (i<j)
+    {
+        while (i<j&&temp<=data[j])
+        {
+            j--;
+        }
+        if(i<j)
+        {
+            data[i]=data[j];
+        }
+        while (i<j&&temp>=data[i])
+        {
+            i++;
+        }
+        if(i<j)
+        {
+            data[j]=data[i];
+        }
+    }
+    data[i]=temp;
+    return i;
+    
+}
+void qksort(int *data,int len)
+{
+    int i,low,height;
+    Qk qk;
+    sStack * stack;
+    stack=StackInit();
+    if(stack)
+    {
+        printf("初始化栈成功\n");
+        qk.low=0;
+        qk.height=len;
+        Push(stack,&qk);
+        while (!StackEmpty())
+        {
+            qk=Pop(stack);
+            low=qk.low;
+            height=qk.height;
+            while (low<height)
+            {
+                i=qkpass(data,low,height);
+                if(i<height)
+                {
+                    qk.low=i+1;
+                    qk.height=height;
+                    Push(stack,qk);
+                }
+                height=i-1;
+            }
+            
+        }
+        
+    }
+    else
+    {
+        printf("初始化栈失败\n");
+    }
+    
+}
---
+++ 快速排序/stack.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "stack.h"
+static const  int StackMask=MAX;
+sStack * StackInit()
+{
+    sStack *stack=NULL;
+    stack=(psStack)malloc(sizeof(sStack));
+    if(stack)
+    {
+        printf("栈创建成功\n");
+        stack->top=NULL;
+        stack->len=0;
+    }
+    return stack;
+}
+void StackDestory(sStack *stack)
+{
+    if(stack)
+    {
+        StackClear(stack);
+        free(stack);
+        stack=NULL;
+        printf("释放栈成功\n");
+    }
+}
+void StackClear(sStack *stack)
+{
+    if(stack)
+    {
+        int len;
+        sStackNode *current,*pre;
+        current=stack->top;
+        len=stack->len;
+        while ((len--)&&current)
+        {
+            pre=current->pre;
+            free(current);
+            current=pre;
+        }
+        stack->len=0;
+    }
+}
+int  StackLength(sStack *stack)
+{
+    if(stack)
+    {
+        return stack->len;
+    }
+    return STACK_ERR;
+}
+bool StackEmpty(sStack *stack)
+{
+    if(stack&&(!stack->len))
+    {
+        return TRUE;
+    }
+    return FALSE;
+    
+}
+bool StackFull(sStack *stack)
+{
+    if(stack&&(stack->len==StackMask))
+    {
+        return TRUE;
+    }
+    return FALSE;
+}
+bool Push(sStack *stack,void * value)
+{
+    if(stack&&stack->len<StackMask)
+    {
+        sStackNode *node=NULL;
+        node =(sStackNode*)malloc(sizeof(sStackNode));
+        if(NULL==node)
+        {
+            return FALSE;
+        }
+        node->data=value;
+        node->pre=stack->top;
+        stack->top=node;
+        stack->len++;
+        return TRUE;
+    }
+    return FALSE;
+}
+void *  Pop(sStack *stack)
+{
+    if(stack&&stack->top>0)
+    {
+        void * data;
+        sStackNode *current;
+        current=stack->top;
+        stack->len--;
+        data=current->data;
+        stack->top=current->pre;
+        free(current);
+        return data;
+    }
+    return STACK_ERR;
+}
+void * GetTop(sStack *stack)
+{
+    if(stack&&stack->top>0)
+    {
+        return (void *)((sStackNode *)(stack->top))->data;
+    }
+    return STACK_ERR;
+}
+void StackDisplay(sStack *stack)
+{
+    if(stack&&stack->top>0)
+    {
+        sStackNode *node=stack->top;
+        int len=stack->len;
+        int i=0;
+        while (len--&&node)
+        {
+            printf("The %d is %d\n",i,node->data);
+            node=node->pre;
+            i++;
+        }        
+    }
+}
